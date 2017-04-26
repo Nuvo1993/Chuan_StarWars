b192df6bbb10212be2c42af89f7d81ec9201ec54
austinneveau <austin.neveau@live.com>
2017-04-25 22:11:23 -0400

    Merge to DEV

--- chuan_finalProject/Controllers/QuizController.php
+++ chuan_finalProject/Controllers/QuizController.php
@@ -31,8 +31,12 @@ class QuizController
             $totalCategoryPoints = 0;
             $userCategoryPoints = 0;
 
+
+
             //echo's display to browser
             echo '<div id="submissionResults">';
+
+            echo '<h1>Results!</h1>';
 
             //get array from within list
             foreach ($list as $categoryArray){
@@ -83,23 +87,215 @@ class QuizController
             //display image based on range of score.
             if($score > 90){
 
-                echo '<div class="resultImg"><img src="../Content/imgs/above90_2.jpg" alt="Above 90%"/></div>';
+                echo '<div class="resultImg">
+                    <div id="Carousel" class="carousel slide" data-ride="carousel">
+                              <!-- Indicators -->
+                              <ol class="carousel-indicators">
+                                <li data-target="#Carousel" data-slide-to="0" class="active"></li>
+                                <li data-target="#Carousel" data-slide-to="1"></li>
+                                <li data-target="#Carousel" data-slide-to="2"></li>
+                                <li data-target="#Carousel" data-slide-to="3"></li>
+                              </ol>
+                            
+                              <!-- Wrapper for slides -->
+                              <div class="carousel-inner" role="listbox">
+                                <div class="item active">
+                                  <img class="carouselImg" src="../Content/imgs/above90/yoda1.jpg" alt="yoda1">
+                                </div>
+                            
+                                <div class="item">
+                                  <img class="carouselImg" src="../Content/imgs/above90/yoda2.jpg" alt="yoda2">
+                                </div>
+                            
+                                <div class="item">
+                                  <img class="carouselImg" src="../Content/imgs/above90/yoda3.jpg" alt="yoda3">
+                                </div>
+                            
+                              </div>
+                            
+                              <!-- Left and right controls -->
+                              <a class="left carousel-control" href="#Carousel" role="button" data-slide="prev">
+                                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                              </a>
+                              <a class="right carousel-control" href="#Carousel" role="button" data-slide="next">
+                                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                              </a>
+                            </div>
+                            <p>You are a Jedo Master! Much knowledge you have.</p>
+                    </div>';
 
             }elseif ($score >= 80 && $score <= 89){
 
-                echo '<div class="resultImg"><img src="../Content/imgs/between80-89.jpg" alt="Between 80% - 89%"/></div>';
+                echo '<div class="resultImg">
+                        <div id="Carousel" class="carousel slide" data-ride="carousel">
+                              <!-- Indicators -->
+                              <ol class="carousel-indicators">
+                                <li data-target="#Carousel" data-slide-to="0" class="active"></li>
+                                <li data-target="#Carousel" data-slide-to="1"></li>
+                                <li data-target="#Carousel" data-slide-to="2"></li>
+                                <li data-target="#Carousel" data-slide-to="3"></li>
+                              </ol>
+                            
+                              <!-- Wrapper for slides -->
+                              <div class="carousel-inner" role="listbox">
+                                <div class="item active">
+                                  <img class="carouselImg" src="../Content/imgs/between80-89/rey1.png" alt="rey1">
+                                </div>
+                            
+                                <div class="item">
+                                  <img class="carouselImg" src="../Content/imgs/between80-89/rey2.jpg" alt="rey2">
+                                </div>
+                            
+                                <div class="item">
+                                  <img class="carouselImg" src="../Content/imgs/between80-89/rey3.jpg" alt="rey3">
+                                </div>
+                            
+                              </div>
+                            
+                              <!-- Left and right controls -->
+                              <a class="left carousel-control" href="#Carousel" role="button" data-slide="prev">
+                                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                              </a>
+                              <a class="right carousel-control" href="#Carousel" role="button" data-slide="next">
+                                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                              </a>
+                            </div>
+                            <p>You are a very strong with the force! But you still have more to learn.</p>
+                <   /div>';
 
             }elseif ($score >= 70 && $score <= 79){
 
-                echo '<div class="resultImg"><img src="../Content/imgs/between70-79.jpg" alt="Between 70% - 79%"/></div>';
+                echo '<div class="resultImg">                        
+                            
+                       <div id="Carousel" class="carousel slide" data-ride="carousel">
+                              <!-- Indicators -->
+                              <ol class="carousel-indicators">
+                                <li data-target="#Carousel" data-slide-to="0" class="active"></li>
+                                <li data-target="#Carousel" data-slide-to="1"></li>
+                                <li data-target="#Carousel" data-slide-to="2"></li>
+                                <li data-target="#Carousel" data-slide-to="3"></li>
+                              </ol>
+                            
+                              <!-- Wrapper for slides -->
+                              <div class="carousel-inner" role="listbox">
+                                <div class="item active">
+                                  <img class="carouselImg" class="carouselImg" src="../Content/imgs/between70-79/obi1.jpg" alt="Obi1">
+                                </div>
+                            
+                                <div class="item">
+                                  <img class="carouselImg" class="carouselImg" src="../Content/imgs/between70-79/obi2.jpg" alt="Obi2">
+                                </div>
+                            
+                                <div class="item">
+                                  <img class="carouselImg" class="carouselImg" src="../Content/imgs/between70-79/between70-79.jpg" alt="Obi3">
+                                </div>
+                            
+                              </div>
+                            
+                              <!-- Left and right controls -->
+                              <a class="left carousel-control" href="#Carousel" role="button" data-slide="prev">
+                                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                              </a>
+                              <a class="right carousel-control" href="#Carousel" role="button" data-slide="next">
+                                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                              </a>
+                            </div>
+                            <p>You are very skilled! You know quite a bit.</p>
+                            </div>';
 
             }elseif ($score >= 60 && $score <= 69){
 
-                echo '<div class="resultImg"><img src="../Content/imgs/between60-69.jpg" alt="Between 60% - 69%"/></div>';
+                echo '<div class="resultImg">
+                        <div id="Carousel" class="carousel slide" data-ride="carousel">
+                              <!-- Indicators -->
+                              <ol class="carousel-indicators">
+                                <li data-target="#Carousel" data-slide-to="0" class="active"></li>
+                                <li data-target="#Carousel" data-slide-to="1"></li>
+                                <li data-target="#Carousel" data-slide-to="2"></li>
+                                <li data-target="#Carousel" data-slide-to="3"></li>
+                              </ol>
+                            
+                              <!-- Wrapper for slides -->
+                              <div class="carousel-inner" role="listbox">
+                                <div class="item active">
+                                  <img class="carouselImg" src="../Content/imgs/between60-69/han1.jpg" alt="han1">
+                                </div>
+                            
+                                <div class="item">
+                                  <img class="carouselImg" src="../Content/imgs/between60-69/han2.png" alt="han2">
+                                </div>
+                            
+                                <div class="item">
+                                  <img class="carouselImg" src="../Content/imgs/between60-69/han1.jpg" alt="han3">
+                                </div>
+                            
+                                <div class="item">
+                                  <img class="carouselImg" src="../Content/imgs/between60-69/han2.png" alt="han4">
+                                </div>
+                              </div>
+                            
+                              <!-- Left and right controls -->
+                              <a class="left carousel-control" href="#Carousel" role="button" data-slide="prev">
+                                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                              </a>
+                              <a class="right carousel-control" href="#Carousel" role="button" data-slide="next">
+                                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                              </a>
+                            </div>
+                        <p>You know enough to get by! You could always use some help though.</p>
+                </div>';
 
             }else{
 
-                echo '<div class="resultImg"><img src="../Content/imgs/below60.jpg" alt="Below 60%"/></div>';
+                echo '<div class="resultImg">
+                           <div id="Carousel" class="carousel slide" data-ride="carousel">
+                              <!-- Indicators -->
+                              <ol class="carousel-indicators">
+                                <li data-target="#Carousel" data-slide-to="0" class="active"></li>
+                                <li data-target="#Carousel" data-slide-to="1"></li>
+                                <li data-target="#Carousel" data-slide-to="2"></li>
+                                <li data-target="#Carousel" data-slide-to="3"></li>
+                              </ol>
+                            
+                              <!-- Wrapper for slides -->
+                              <div class="carousel-inner" role="listbox">
+                                <div class="item active">
+                                  <img class="carouselImg" src="../Content/imgs/below60/jar1.jpg" alt="jar1">
+                                </div>
+                            
+                                <div class="item">
+                                  <img class="carouselImg" src="../Content/imgs/below60/jar.png" alt="jar2">
+                                </div>
+                            
+                                <div class="item">
+                                  <img class="carouselImg" src="../Content/imgs/below60/jar_3.jpg" alt="jar3">
+                                </div>
+                            
+                                <div class="item">
+                                  <img class="carouselImg" src="../Content/imgs/below60/jar4.jpg" alt="jar4">
+                                </div>
+                              </div>
+                            
+                              <!-- Left and right controls -->
+                              <a class="left carousel-control" href="#Carousel" role="button" data-slide="prev">
+                                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                              </a>
+                              <a class="right carousel-control" href="#Carousel" role="button" data-slide="next">
+                                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                              </a>
+                            </div>
+                            <p>Jar Jar! You have much to learn...</p>
+                        </div>';
 
             }
 
--- chuan_finalProject/Scripts/scripts.js
+++ chuan_finalProject/Scripts/scripts.js
@@ -2,21 +2,8 @@
  * Created by austinneveau on 4/3/17.
  */
 
-function hideDiv(){
-    setTimeout(function() {
-    }, 10000);
-}
 
-setInterval(function(){
-    $('#demo').fadeOut();
-},15000);
+setTimeout(function(){
+    $('#starWarsIntro').fadeOut();
+},120000);
 
-
-function hideShowDiv() {
-    var x = document.getElementById('.starterTemplate');
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
-}
